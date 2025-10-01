fc97a3b2b48ec47aee7bece528603e16392b3bf4
Trystan Henriques <trystanhenriquesgit@gmail.com>
2025-10-01 17:15:56 -0400

    Created full implementation of arr_get_value (#3)

--- src/array.asm
+++ src/array.asm
@@ -6,14 +6,43 @@ OPTION prologue:none, epilogue:none     ; we use SAFE_PROLOGUE/SAFE_EPILOGUE
 INCLUDE array.inc
 
 .code
-; -------------------------
-; arr_get_value(base,len,i)
-; Success: CF=0, RAX=value
-; Errors:  CF=1, EAX=MYLIB_ERR_*
+
+;________________________________________
+; arr_get_value(base,len,index)
+;________________________________________
+; Returns:
+;   CF=0, RAX = value            ; success
+;   CF=1, EAX = ARR_ERR_*        ; error
+; Errors:
+;   ARR_ERR_NULLPTR   if base == NULL
+;   ARR_ERR_LEN_ZERO  if len  == 0
+;   ARR_ERR_OUTOFRANGE if index >= len
+;________________________________________
 arr_get_value PROC base:QWORD, len:QWORD, index:QWORD
     SAFE_PROLOGUE
-    ; body pending
-    RET_ERR ARR_ERR_NULLPTR   ; placeholder so file assembles; remove when implemented
+    ; RCX=base, RDX=len, R8=index  (Win64)
+
+    ; base must be non-NULL
+    test rcx, rcx
+    jnz  _base_ok
+      RET_ERR ARR_ERR_NULLPTR
+_base_ok:
+
+    ; len must be > 0
+    test rdx, rdx
+    jnz  _len_ok
+      RET_ERR ARR_ERR_LEN_ZERO
+_len_ok:
+
+    ; 0 <= index < len
+    cmp  r8, rdx
+    jb   _in_range
+      RET_ERR ARR_ERR_OUTOFRANGE
+_in_range:
+
+    ; load QWORD element
+    mov  rax, [rcx + r8*8]
+    RET_OK
 arr_get_value ENDP
 
 arr_set_value PROC base:QWORD, len:QWORD, index:QWORD, value:QWORD
