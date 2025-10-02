4f6fb5274f645f1a0901a697ba688dd7e8fe39db
Trystan Henriques <trystanhenriquesgit@gmail.com>
2025-10-02 13:55:27 -0400

    Implemented arr_index_of_smax PROC

--- src/array.asm
+++ src/array.asm
@@ -573,5 +573,60 @@ _ok:
     RET_OK
 arr_smin ENDP
 
+;_______________________________________________________
+; arr_index_of_smax(base,len)
+;_______________________________________________________
+; Returns:
+;   CF=0, RAX = index of signed max (two's-complement; first occurrence on ties)
+;   CF=1, EAX = ARR_ERR_*        ; error
+; Errors:
+;   ARR_ERR_NULLPTR  if base == NULL
+;   ARR_ERR_LEN_ZERO if len  == 0
+;_______________________________________________________
+arr_index_of_smax PROC base:QWORD, len:QWORD
+    SAFE_PROLOGUE
+    ; Win64: RCX=base, RDX=len
+
+    ; base must be non-NULL
+    test rcx, rcx
+    jnz  _base_ok
+      RET_ERR ARR_ERR_NULLPTR
+_base_ok:
+
+    ; len must be > 0
+    test rdx, rdx
+    jnz  _len_ok
+      RET_ERR ARR_ERR_LEN_ZERO
+_len_ok:
+
+    ; current max value in R8 (signed), index in R10
+    mov  r8,  [rcx]          ; max value
+    xor  r10, r10            ; max index = 0
+
+    cmp  rdx, 1
+    je   _ret_ok             ; len==1 -> index 0
+
+    lea  r9,  [rcx+8]        ; ptr to next element
+    mov  r11, 1              ; loop index i = 1
+    mov  rax, rdx
+    dec  rax                 ; remaining = len-1
+
+_loop:
+    mov  rdx, [r9]           ; candidate
+    cmp  rdx, r8
+    jle  _skip               ; signed: update only if candidate > current max
+    mov  r8,  rdx
+    mov  r10, r11
+_skip:
+    add  r9, 8
+    inc  r11
+    dec  rax
+    jnz  _loop
+
+_ret_ok:
+    mov  rax, r10            ; return index
+    RET_OK
+arr_index_of_smax ENDP
+
 
 END
